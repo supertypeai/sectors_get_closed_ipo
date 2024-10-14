865ec27648b66173aeb44b73fb362c0759539dda
aidityas <aidityasadhakim250@gmail.com>
2024-10-14 12:48:02 +0700

    fix(logging): Add more detail logging info, which log what symbols is being updated and how many symbols are updated successfully

--- main.py
+++ main.py
@@ -21,19 +21,6 @@ def initiate_logging(LOG_FILENAME):
     logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO, format=formatLOG)
     logging.info('Program started')
 
-
-LOG_FILENAME = 'scraper.log'
-initiate_logging(LOG_FILENAME)
-
-PROXY = os.getenv("PROXY")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-proxy_support = urllib.request.ProxyHandler({'http': PROXY,'https': PROXY})
-opener = urllib.request.build_opener(proxy_support)
-urllib.request.install_opener(opener)
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
 def extract_company_info(new_url):
     try:
         with urllib.request.urlopen(new_url) as response:
@@ -66,73 +53,89 @@ def extract_company_info(new_url):
         print(f"Error extracting company info: {str(e)}")
         return {}
 
-result = {
-    "symbol" : [],
-    "ipo_price" : [],
-    "underwriter": [],
-    "updated_on": [],
-    "href" : [],
-}
+if __name__ == '__main__':
+    LOG_FILENAME = 'scraper.log'
+    initiate_logging(LOG_FILENAME)
 
-update_data = {
-    "symbol" : [],
-    "ipo_price" : [],
-    "underwriter": [],
-    "updated_on": [],
-}
+    PROXY = os.getenv("PROXY")
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-try:
-    url = f'https://e-ipo.co.id/en/ipo/index?page=1&per-page=&query=&sort=-updated_at&status_id=5&view=list'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print(html)
-    soup = BeautifulSoup(html, 'html.parser')
-    names = []
-    names_class = soup.find_all(class_="margin-left10 colorwhite")
-    for name in names_class:
-        company_name, symbol = name.get_text().replace(" Sharia", "").replace(")", "").split(' (')
-        result["symbol"].append(symbol.replace("Closed", "").replace("Book Building","") + ".JK")
-    notopmargins = soup.find_all("p", class_="notopmargin")
-    nobottommargins = soup.find_all(class_="nobottommargin")
-    for top, bottom in zip(notopmargins, nobottommargins):
-        if bottom.get_text() == "Final Price": result["ipo_price"].append(top.get_text().replace("IDR\xa0", ""))
-    buttons = soup.find_all(class_="button button-3d button-small notopmargin button-rounded button-dirtygreen")
-    for button in buttons:
-        result["href"].append(button.get("href"))
-    
+    proxy_support = urllib.request.ProxyHandler({'http': PROXY,'https': PROXY})
+    opener = urllib.request.build_opener(proxy_support)
+    urllib.request.install_opener(opener)
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+    result = {
+        "symbol" : [],
+        "ipo_price" : [],
+        "underwriter": [],
+        "updated_on": [],
+        "href" : [],
+    }
+
+    update_data = {
+        "symbol" : [],
+        "ipo_price" : [],
+        "underwriter": [],
+        "updated_on": [],
+    }
+
     try:
-        company_ipo_price_null = supabase.table('idx_company_profile').select('symbol').filter('ipo_price', 'is', 'null').execute().data
-        company_symbols_null_ipo = [d['symbol'] for d in company_ipo_price_null]
-    except Exception as e:
-        print(f"An exception when supabase: {str(e)}")
+        url = f'https://e-ipo.co.id/en/ipo/index?page=1&per-page=&query=&sort=-updated_at&status_id=5&view=list'
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        names = []
+        names_class = soup.find_all(class_="margin-left10 colorwhite")
+        for name in names_class:
+            company_name, symbol = name.get_text().replace(" Sharia", "").replace(")", "").split(' (')
+            result["symbol"].append(symbol.replace("Closed", "").replace("Book Building","") + ".JK")
+        notopmargins = soup.find_all("p", class_="notopmargin")
+        nobottommargins = soup.find_all(class_="nobottommargin")
+        for top, bottom in zip(notopmargins, nobottommargins):
+            if bottom.get_text() == "Final Price": result["ipo_price"].append(top.get_text().replace("IDR\xa0", ""))
+        buttons = soup.find_all(class_="button button-3d button-small notopmargin button-rounded button-dirtygreen")
+        for button in buttons:
+            result["href"].append(button.get("href"))
         
-    try:
-        for symbol in result["symbol"]:
-            if symbol in company_symbols_null_ipo:
-                idx = result["symbol"].index(symbol)
-                new_url = f"https://e-ipo.co.id{result['href'][idx]}"
-                company_info = extract_company_info(new_url)
-                update_data["symbol"].append(symbol)
-                update_data["ipo_price"].append(result["ipo_price"][idx])
-                update_data["underwriter"].append(company_info['Underwriter(s)'])
-                now = datetime.now()
-                update_data["updated_on"].append(now.strftime("%Y-%m-%d %H:%M:%S"))
-    except Exception as e:
-        print(f"An exception when retrieve data: {str(e)}")
-        
-    # Updating ipo price
-    for symbol, ipo_price, underwriter, updated_on in zip(update_data["symbol"], update_data["ipo_price"], update_data["underwriter"], update_data["updated_on"]):
         try:
-            supabase.table('idx_company_profile').update({
-                'ipo_price': ipo_price,
-                'underwriter': underwriter,
-                'updated_on': updated_on
-                }).eq('symbol', symbol).execute()
-            print("Symbol updated successfully for: ", symbol)
+            company_ipo_price_null = supabase.table('idx_company_profile').select('symbol').filter('ipo_price', 'is', 'null').execute().data
+            company_symbols_null_ipo = [d['symbol'] for d in company_ipo_price_null]
         except Exception as e:
-            print(f"Error updating data: {str(e)}")
+            print(f"An exception when supabase: {str(e)}")
+            
+        try:
+            for symbol in result["symbol"]:
+                if symbol in company_symbols_null_ipo:
+                    idx = result["symbol"].index(symbol)
+                    new_url = f"https://e-ipo.co.id{result['href'][idx]}"
+                    company_info = extract_company_info(new_url)
+                    update_data["symbol"].append(symbol)
+                    update_data["ipo_price"].append(result["ipo_price"][idx])
+                    update_data["underwriter"].append(company_info['Underwriter(s)'])
+                    now = datetime.now()
+                    update_data["updated_on"].append(now.strftime("%Y-%m-%d %H:%M:%S"))
+        except Exception as e:
+            print(f"An exception when retrieve data: {str(e)}")
+            logging.info(f"An exception when retrieve data: {str(e)}")
+            
+        # Updating ipo price
+        for symbol, ipo_price, underwriter, updated_on in zip(update_data["symbol"], update_data["ipo_price"], update_data["underwriter"], update_data["updated_on"]):
+            try:
+                supabase.table('idx_company_profile').update({
+                    'ipo_price': ipo_price,
+                    'underwriter': underwriter,
+                    'updated_on': updated_on
+                    }).eq('symbol', symbol).execute()
+                print("Symbol updated successfully for: ", symbol)
+                logging.info(f"Symbol updated successfully for: {symbol}")
+            except Exception as e:
+                print(f"Error updating data: {str(e)}")
+                logging.info(f"Error updating data: {str(e)}")
 
-except Exception as e:
-    print(f"An exception occurred: {str(e)}")
+    except Exception as e:
+        print(f"An exception occurred: {str(e)}")
+        logging.info(f"An exception occurred: {str(e)}")
 
-logging.info(f"Finish scrape closed ipo data")
+    logging.info(f"Finish scrape {len(update_data['ipo_price'])} closed ipo data")
